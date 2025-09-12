4069098a6d55761f74c71462297d0bf8dc973ebe
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-09-12 19:07:51 +0800

    commit pertama di folder go dasar wpu

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dasar-go-wpu
+
+go 1.24.5
---
+++ latihan-variable/variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string = "fadillah"
+	var umur int = 16
+
+	const pi = 3.14
+	const app = "Belajar golang dasar"
+
+	alamat := "sungai jingah"
+	Jurusan := "Rekayasa Perangkat Lunak"
+
+	fmt.Println("Nama : ", nama)
+	fmt.Println("Umur : ", umur)
+	fmt.Println("Alamat : " ,alamat)
+	fmt.Println("Jurusan : ", Jurusan)
+	fmt.Println("PI : ", pi)
+	fmt.Println("appName : ", app)
+}

6f6056d4224c86372a478b3d2a457bbf5fd2a470
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-09-30 11:08:10 +0800

    belajar cara penggunaan operator asterisk

---
+++ asterisk_operator/asterisk.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Addres struct {
+	City, Province, Country string
+}
+
+func main() {
+	addres1 := Addres{"Subang", "Jawa barat", "Indonesia"}
+	addres2 := &addres1 // pointer
+
+	addres2.City = "Bandung"
+	fmt.Println(addres1) // Ikut berubah
+	fmt.Println(addres2) // data yang berubah
+
+	*addres2 = Addres{"Banjar","kalsel","Indonesia"}
+	fmt.Println(addres1)
+	fmt.Println(addres2)
+}
